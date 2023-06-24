f656456bc9d868ca919d808e7e2dba1fd8ba12f3
LaxRaj <lakshya@Lakshyarajs-MacBook-Air.local>
2023-06-23 22:20:38 -0700

    codingbat warmup 2

---
+++ frontTimes/fronttimes.java
@@ -0,0 +1,20 @@
+package frontTimes;
+
+public class fronttimes {
+    public String frontTimes(String str, int n) {
+  if (str.length() == 0 ) {return str;}
+  if ( n == 0 ) {return "";}
+  if (str.length() <= 3 ) { 
+    String res = "";
+    for (int i=0; i < n ; i++) {
+      res = res + str;
+    }
+    return res ; }
+    str = str.substring(0, 3);
+    String res = "";
+    for (int i=0; i < n ; i++) {
+      res = res + str;
+    }
+  return res;
+}
+}
---
+++ stringTimes/stringtimes.java
@@ -0,0 +1,12 @@
+package stringTimes;
+
+public class stringtimes {
+    public String stringTimes(String str, int n) {
+  String res = "";
+  for (int i =0 ; i < n ; i++) {
+      res = res+str;
+  }
+  return res;
+}
+
+}
